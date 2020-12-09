acfb6b7d6a0146268e156c8acdbf254c2e03de76
LUIS DEL MORAL TÉBAR <LuisDel.Moral@alu.uclm.es>
2020-12-10 00:29:57 +0100

    preparación para despliegue

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var bodyParser = require("body-parser");
-var io = require('socket.io').listen(server);
+var io = require("socket.io").listen(server);
 
 var modelo=require("./servidor/modelo.js");
 var wss=require("./servidor/servidorWS.js");
